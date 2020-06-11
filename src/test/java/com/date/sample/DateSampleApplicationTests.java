c3174a42823811d72aa15e3ff31a1ef59497d97b
Ryo Takanami <rtakanami0706@gmail.com>
2020-06-11 17:31:24 +0900

    First commit

---
+++ src/test/java/com/date/sample/DateSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.date.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
